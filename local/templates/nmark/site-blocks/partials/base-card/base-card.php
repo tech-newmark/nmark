a538a91529e5fa25ac867ed1d9316c718c8223fd
Oksana Tarasova <oksanchik_90@mail.ru>
2026-02-17 19:31:13 +0300

    Переношу базовую карточку в отдельный компонент

--- local/templates/nmark/site-blocks/partials/base-card/base-card.php
+++ local/templates/nmark/site-blocks/partials/base-card/base-card.php
@@ -7,8 +7,5 @@ $stylesIncluded['base-card'] = true;
 
 <div class="base-card">
     <p class="base-card__name"><?= $arItem['NAME'] ?></p>
-    <p class="base-card__desc">
-        <strong class="strong"><?= $arItem['PREVIEW_TEXT'] ?></strong>
-        <?= $arItem['DETAIL_TEXT'] ?>
-    </p>
+    <p class="base-card__desc"><?= $arItem['PREVIEW_TEXT'] ?></p>
 </div>
