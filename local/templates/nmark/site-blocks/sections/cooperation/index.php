2cf94ff16eecd31202473d1164d1a19343099a59
Oksana Tarasova <oksanchik_90@mail.ru>
2026-02-17 12:41:50 +0300

    Стилизация баннера о сотрудничестве

--- local/templates/nmark/site-blocks/sections/cooperation/index.php
+++ local/templates/nmark/site-blocks/sections/cooperation/index.php
@@ -5,11 +5,21 @@ use Bitrix\Main\Page\Asset;
 Asset::getInstance()->addCss(SITE_TEMPLATE_PATH . "/site-blocks/sections/cooperation/style.css");
 ?>
 
-<div class="base-section cooperation">
-    <div class="container">
-        <p class="cooperation__slogan">Вместе успешнее!</p>
-        <p class="cooperation__text">Давайте работать<br>
-            <span>вместе</span> <button class="main-btn main-btn--secondary">Нажмите</button>
-        </p>
+<div class="base-section ">
+    <div class="cooperation">
+        <div class="container">
+            <div class="cooperation__content">
+                <p>Вместе успешнее!</p>
+                <p>Давайте работать
+                    <span>вместе
+                        <button class="main-btn main-btn--secondary">Нажмите
+                            <svg width="48" height="48" role="img" aria-hidden="true" focusable="false">
+                                <use xlink:href="<?= SITE_TEMPLATE_PATH ?>/assets/sprite.svg#icon-arrow"></use>
+                            </svg>
+                        </button>
+                    </span>
+                </p>
+            </div>
+        </div>
     </div>
 </div>
